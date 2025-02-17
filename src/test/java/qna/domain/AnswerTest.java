cfcadcf316b8ba5ed1c575a72c81b60766355496
HYOJIN <hjhwang08@naver.com>
2025-02-17 17:06:39 +0900

    feat: unit test

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,36 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.exception.CannotDeleteException;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.CancellationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.DORAEMON, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SPONGEBOB, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    public void 다른_사람이_쓴_답변이_존재(){
+        //when
+        A1.isOwner(UserTest.DORAEMON);
+
+        assertThatThrownBy(() -> A1.isAnswerLoginUser(UserTest.SPONGEBOB))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    public void answer_삭제_성공(){
+        //when
+        A1.isOwner(UserTest.DORAEMON);
+
+        //given
+        DeleteHistory answer삭제 = A1.delete();
+        DeleteHistory answer삭제결과 = new DeleteHistory(ContentType.ANSWER, A1.getId(), A1.getWriter(), LocalDateTime.now());
+
+        assertThat(answer삭제).isEqualTo(answer삭제결과);
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,20 +1,36 @@
 package qna.domain;
 
 import org.junit.jupiter.api.Test;
+import qna.exception.CannotDeleteException;
+import java.time.LocalDateTime;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import static org.mockito.Mockito.when;
 
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.DORAEMON);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SPONGEBOB);
 
     @Test
-    public void delete_다른_사람이_쓴_질문(){
+    public void delete_다른_사람이_올린_질문(){
+        //when
+        Q1.isOwner(UserTest.DORAEMON);
 
+        assertThatThrownBy(() -> Q1.isQuestionLoginUser(UserTest.SPONGEBOB))
+                .isInstanceOf(CannotDeleteException.class);
     }
 
+    @Test
+    public void question_delete_성공(){
+        //when
+        Q1.isOwner(UserTest.DORAEMON);
 
+        //given
+        DeleteHistory question삭제 = Q1.delete();
+        DeleteHistory question삭제결과 = new DeleteHistory(ContentType.QUESTION, Q1.getId(), Q1.getWriter(), LocalDateTime.now());
 
+        assertThat(question삭제).isEqualTo(question삭제결과);
+    }
 
 
 
